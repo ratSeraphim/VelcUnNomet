eb9fc6fff85cf91cae12b561a326e8e4f196f0a1
ratSeraphim <telmy2458@hotmail.lv>
2021-04-06 11:43:07 +0300

    Uzsākta pirmā skripta rakstīšana

---
+++ Assets/Skripti/DragDropSkripts.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Jāimportē, lai varētu strādāt ar EventSystems
+using UnityEngine.EventSystems;
+
+public class DragDropSkripts : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler /*, IEndDragHandler*/
+{
+    //Uzglabās norādi uz Objektu skriptu
+   // public Objekti objektuSkripts;
+    //Velkamajam objektam piestiprinātā komponente
+    private CanvasGroup kanvasGrupa;
+    //Vilktā objekta atrašanās vietas koordinātu maiņai
+    private RectTransform velkObjRectTransf;
+
+    void Awake()
+    {
+        //Piekļūst objektam piesaistītajai CanvasGroup komponentei
+        kanvasGrupa = GetComponent<CanvasGroup>();
+        //Piekļūst objekta RectTransform komponentei
+        velkObjRectTransf = GetComponent<RectTransform>();
+    }    
+
+    //Nostrādā nospiežot peles klikšķi uz objekta
+    public void OnPointerDown(PointerEventData notikums)
+    {
+        Debug.Log("Uzklikšķināts uz velkamā objekta");
+    }
+
+    //Nostrādā uzsākot vilkšanu
+    public void OnBeginDrag(PointerEventData notikums)
+    {
+        Debug.Log("Uzsākta vilkšana!");
+        //Attīra pēdējo vilkto objektu
+        //objektuSkripts.pedejaisVilktais = null;
+        //Uzsākot vilkt objektu tas paliek nedaudz caurspīdīgs
+        kanvasGrupa.alpha = 0.6f;
+        //Lai objektam varētu iet cauri raycasst stars
+        kanvasGrupa.blocksRaycasts = false;
+
+    }
+
+    //Nostrādā reāli notiekot vilkšanai
+    public void OnDrag(PointerEventData notikums)
+    {
+        Debug.Log("Notiek vilkšana!");
+      // velkObjRectTransf.anchoredPosition += notukums.delta / objektuSkripts.kanva.scaleFactor;
+    }
+}
+
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # VelcUnNomet
 ## Veicamo darbu saraksts
 - [x] Izveidot GitHub repo
-- [ ] Sagatavot Unity projektu darba uzsākšanai (mapes, bildes, skaņas)
+- [x] Sagatavot Unity projektu darba uzsākšanai (mapes, bildes, skaņas)
 - [ ] Realizēt objektu vilkšanu un nomešanu (uz kartes, pareizi, nepareizi)
 - [ ] Realizēt objektu transformācijas (izmēra maiņa, rotācija)
 - [ ] Realizēt laika atskaiti un uzvaras logu (laiks, zvaigznītes)
